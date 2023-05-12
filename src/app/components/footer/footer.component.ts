fb27ec35101856041abca798bb1f096b04c0ef2c
Guilherme Henrique de Oliveira <guihenrique_oliveira@hotmail.com>
2023-05-11 22:02:06 -0300

    Fix minor bugs

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -10,10 +10,12 @@ export class FooterComponent {
   github: string = `guilherme-de-oliveira`;
   linkedin: string =`guilherme-h-de-oliveira`;
   email: string =`guihenrique_oliveira@hotmail.com`;
+  medium: string =`@guilherme-de-oliveira`;
 
   items: MenuItem[] = [
     {title: 'Github', label: `${this.github}`, icon: 'pi pi-fw pi-github', url: 'https://github.com/guilherme-de-oliveira'},
     {title: 'Linkedin', label: `${this.linkedin}`, icon: 'pi pi-fw pi-linkedin', url: 'https://www.linkedin.com/in/guilherme-h-de-oliveira'},
     {title: 'Email', label: `${this.email}`, icon: 'pi pi-fw pi-id-card', url: 'guihenrique_oliveira@hotmail.com'},
+    {title: 'Medium', label: `${this.medium}`, icon: 'pi pi-fw pi-id-card', url: 'https://medium.com/@guilherme-de-oliveira'},
   ];
 }
